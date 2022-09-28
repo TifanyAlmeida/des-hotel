ad82af979dbb4e3f44b451707eb7e704cf53c49b
TifanyAlmeida <Ticaanahi@outlook.com>
2022-09-28 17:00:22 -0300

    28-09-2022 17:00

--- home/models.py
+++ home/models.py
@@ -1,3 +1,5 @@
+from secrets import choice
+from unicodedata import decimal
 from django.db import models
 
 # arrumar o vista para que seja possivel escolher mais de um
@@ -30,7 +32,6 @@ class ListaRecurso(models.Model):
 #RECURSO
 class Recurso(models.Model):
 
-    # recursos_local = models.CharField(max_length=2, choices=RECURSOS_LOCAL_CHOICE, default=VISTA)
     nome_recurso = models.CharField(max_length=60)
     lista_recurso = models.ManyToManyField(ListaRecurso)
 
@@ -38,13 +39,26 @@ class Recurso(models.Model):
         return self.nome_recurso
 
 #lembrar de botar cascacade
+D = 'Dia'
+S = 'Semana'
+M = 'Mês'
+PERIODO_CHOICES = [
+    (D, 'Dia'),
+    (S, 'Semana'),
+    (M, 'Mês'),
+]
 
 class Hospedagen(models.Model):
     titulo = models.CharField(max_length=50)
     descricao = models.CharField(max_length=150)
+    preco = models.DecimalField(decimal_places=2, max_digits=8)
+    preco_por = models.CharField(choices=PERIODO_CHOICES, max_length=6, default=D)
+    taxa_limpeza = models.DecimalField(decimal_places=2, max_digits=8)
     imagem_1 = models.ImageField(upload_to='foto/%y/%m/%d')
     imagem_2 = models.ImageField(upload_to='foto/%y/%m/%d')
     imagem_3 = models.ImageField(upload_to='foto/%y/%m/%d')
+    imagem_4 = models.ImageField(upload_to='foto/%y/%m/%d')
+    imagem_5 = models.ImageField(upload_to='foto/%y/%m/%d')
     disponibilidade_a_partir = models.DateField()
     endereco = models.ForeignKey(Endereco, on_delete=models.PROTECT)
     lista_recurso = models.ForeignKey(Recurso, on_delete=models.PROTECT)
--- home/views.py
+++ home/views.py
@@ -1,6 +1,6 @@
 from gc import get_objects
 from django.shortcuts import render, get_object_or_404
-from .models import Hospedagen
+from .models import Hospedadore, Hospedagen, Recurso
 
 def mostrar_home(request):
     banco_hotel = Hospedagen.objects.all()
@@ -13,4 +13,10 @@ def detalhes(request, id):
     det_hotel = get_object_or_404(
         Hospedagen, id=id
     )
-    return render(request,'home/detalhes.html', {'detalhes':det_hotel})
+    det_hospedador = get_object_or_404(
+        Hospedadore, id=id
+    )
+    det_recursos = get_object_or_404(
+        Recurso, id=id
+    )
+    return render(request,'home/detalhes.html', {'detalhes':det_hotel, 'detalhes_hosped':det_hospedador, 'detalhes_recursos':det_recursos})
